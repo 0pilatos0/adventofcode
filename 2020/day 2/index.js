b06365e786ad4fa73b9b09ba82fa354d1f2e39f9
0pilatos0 <paulvanderlei@hotmail.nl>
2021-11-22 10:28:31 +0100

    day 2 done

---
+++ 2020/day 2/index.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var data = fs.readFileSync('data').toString().split('\n');
+var correct = 0;
+for (var i = 0; i < data.length; i++) {
+    data[i] = data[i].replace(/\r/g, '');       
+}
+
+for (var i = 0; i < data.length; i++) {
+    
+    var min = data[i].split('-')[0];
+    var max = data[i].split('-')[1];
+    var min = min.split(' ')[0];
+    var max = max.split(' ')[0];
+    min = parseInt(min);
+    max = parseInt(max);
+
+    var letter = data[i].split(' ')[1].split('')[0];
+
+    var password = data[i].split(' ')[2];
+    var password = password.split('');
+
+    var templetter = 0;
+    
+    /* First part */
+    // for (var j = 0; j < password.length; j++) {
+    //     if (password[j] === letter) {
+    //         templetter++;
+    //     }
+    // }
+    
+    // if (templetter >= min && templetter <= max) {
+    //     correct++;
+    // }
+
+    /* Second part */
+    if (password[min - 1] === letter) {
+        templetter++;
+    }
+    if (password[max - 1] === letter) {
+        templetter++;
+    }
+    if (templetter === 1){
+        correct++;
+    }
+
+}
+console.log(correct);
+
