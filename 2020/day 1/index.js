66305273ba67df151d48f073c84f2ab7b2291f55
0pilatos0 <paulvanderlei@hotmail.nl>
2021-11-22 09:57:17 +0100

    new file structure

---
+++ 2020/day 1/index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var data = fs.readFileSync('data');
+var data = data.toString();
+var data = data.split('\n');
+
+for (var i = 0; i < data.length; i++) {
+    data[i] = data[i].replace(/\r/g, '');    
+    data[i] = parseInt(data[i]);
+}
+
+/* First part */
+// for(var a = 0; a < data.length; a++) {
+//     for(var b = 0; b < data.length; b++) {
+//         let calculated = data[a] + data[b];
+//         if(calculated === 2020){
+//             console.log(data[a] * data[b]);
+//         }
+//     }
+// }
+
+/* Second part */
+for(var a = 0; a < data.length; a++) {
+    for(var b = 0; b < data.length; b++) {
+        for(var c = 0; c < data.length; c++) {
+            let calculated = data[a] + data[b] + data[c];
+            if(calculated === 2020){
+                console.log(data[a] * data[b] * data[c]);
+            }
+        }
+    }
+}
