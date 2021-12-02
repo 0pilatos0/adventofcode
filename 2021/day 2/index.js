56f413eba9d2619705e5f74b8fa125dbedb7696c
0pilatos0 <paulvanderlei@hotmail.nl>
2021-12-02 09:01:50 +0100

    refactoring day 2

--- 2021/day 2/index.js
+++ 2021/day 2/index.js
@@ -3,20 +3,40 @@ const input = fs.readFileSync('input.txt', 'utf8').split('\r\n');
 
 let xpos = 0;
 let ypos = 0;
-let aim = 0;
-for(let i = 0; i < input.length; i++) {
-    input[i] = input[i].split(' ');
-    let direction = input[i][0];
-    let distance = parseInt(input[i][1]);
 
-    if(direction === 'forward'){
-        xpos += distance;
-        ypos += (aim * distance);
-    } else if(direction === 'down'){
-        aim += distance;
+// part1();
+// part2();
 
-    } else if(direction === 'up'){
-        aim -= distance;
+function part1(){
+    for(let i = 0; i < input.length; i++) {
+        input[i] = input[i].split(' ');
+        let direction = input[i][0];
+        let distance = parseInt(input[i][1]);
+        if(direction === 'forward'){
+            xpos += distance;
+        } else if(direction === 'down'){
+            ypos += distance;
+        } else if(direction === 'up'){
+            ypos -= distance;
+        }
+    }
+}
+
+
+function part2(){
+    let aim = 0;
+    for(let i = 0; i < input.length; i++) {
+        input[i] = input[i].split(' ');
+        let direction = input[i][0];
+        let distance = parseInt(input[i][1]);
+        if(direction === 'forward'){
+            xpos += distance;
+            ypos += (aim * distance);
+        } else if(direction === 'down'){
+            aim += distance;
+        } else if(direction === 'up'){
+            aim -= distance;
+        }
     }
 }
 console.log(xpos * ypos);
