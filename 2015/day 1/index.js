339169ea7677c9ed7cbc1c9689241025e3e586bb
Paul van der Lei <paul.vanderlei@wics.nl>
2022-05-20 14:30:49 +0200

    finished day 1

---
+++ 2015/day 1/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf8").trim().split("");
+
+function part1() {
+  let floor = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "(") {
+      floor++;
+    } else if (input[i] === ")") {
+      floor--;
+    }
+  }
+  return floor;
+}
+
+function part2() {
+  let floor = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "(") {
+      floor++;
+    } else if (input[i] === ")") {
+      floor--;
+    }
+    if (floor < 0) {
+      return i + 1;
+    }
+  }
+
+  return "Santa is not in the basement";
+}
+
+console.log(part1());
+console.log(part2());
