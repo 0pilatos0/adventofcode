52acb0c2e0c8a8cd6d478de6aa187da79cf027b6
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-12-01 07:02:20 +0100

    🎄finished day 1 2022

--- 2022/day 1/index.js
+++ 2022/day 1/index.js
@@ -6,18 +6,50 @@ const input = fs.readFileSync(inputFile, 'utf8').split('\n');
 
 
 /* PART 1 */
-function part1(){
-    //TODO - implement
+function part1() {
+    let mostCalories = 0;
+    let currentCalories = 0;
+
+    input.forEach(calorie => {
+        calorie = parseInt(calorie);
+
+        if (currentCalories > mostCalories) {
+            mostCalories = currentCalories;
+        }
+
+        if (isNaN(calorie)) {
+            currentCalories = 0;
+        } else {
+            currentCalories += calorie;
+        }
+    });
+
+    return mostCalories;
 }
 
 
 /* PART 2 */
-function part2(){
-    //TODO - implement
+function part2() {
+    let mostCalories = [0, 0, 0]
+    let currentCalories = 0;
+
+    input.forEach(calorie => {
+        calorie = parseInt(calorie);
+
+        if (isNaN(calorie)) {
+            let lowest = mostCalories.indexOf(Math.min(...mostCalories));
+            if (currentCalories > mostCalories[lowest]) {
+                mostCalories[lowest] = currentCalories;
+            }
+
+            currentCalories = 0;
+        } else {
+            currentCalories += calorie;
+        }
+    });
+    return mostCalories.reduce((a, b) => a + b, 0);
 }
 
 
 console.log("Part 1: " + part1());
 console.log("Part 2: " + part2());
-
-
