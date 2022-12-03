551750ef9120659b1610b1bed42503bb8b674562
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-12-03 07:47:58 +0100

    🎄finished day 3 2022

--- 2022/day 3/index.js
+++ 2022/day 3/index.js
@@ -7,13 +7,68 @@ const input = fs.readFileSync(inputFile, 'utf8').split('\n');
 
 /* PART 1 */
 function part1() {
-   
+
+    let dupedLetters = [];
+    let totalpoints = 0;
+    let alfabeth = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    input.forEach(line => {
+        let firstHalf = line.slice(0, line.length / 2).split('');
+        let secondHalf = line.slice(line.length / 2, line.length).split('');
+
+
+        let rowDupedLetters = [];
+
+        for (let i = 0; i < firstHalf.length; i++) {
+            if (secondHalf.includes(firstHalf[i])) {
+                if (!rowDupedLetters.includes(firstHalf[i])) {
+                    rowDupedLetters.push(firstHalf[i]);
+                }
+            }
+        }
+        dupedLetters.push(rowDupedLetters);
+    });
+
+    dupedLetters.forEach(letter => {
+        letter.forEach(l => {
+            totalpoints += alfabeth.indexOf(l) + 1;
+        });
+    });
+
+    return totalpoints;
+
 }
 
 
 /* PART 2 */
 function part2() {
-   
+    let points = 0;
+    let badges = [];
+    let alfabeth = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    for (let i = 0; i < input.length; i += 3) {
+
+        let badge = "";
+
+        let first = input[i].trim().split('');
+        let second = input[i + 1].trim().split('');
+        let third = input[i + 2].trim().split('');
+
+        first.forEach((letter, index) => {
+            if (second.includes(letter) && third.includes(letter)) {
+                badge = letter;
+            }
+        });
+
+        if (badge !== "") {
+            badges.push(badge);
+        }
+    }
+
+    badges.forEach(letter => {
+        points += alfabeth.indexOf(letter) + 1;
+    });
+
+    return points;
 }
 
 
