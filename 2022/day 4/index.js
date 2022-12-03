1bcf587b7dab218f3ffc6995bc18efac983f9bc6
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-12-03 07:48:41 +0100

    🎄prepared day 4 2022

---
+++ 2022/day 4/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// const inputFile = "input.txt";
+const inputFile = "testInput.txt";
+const input = fs.readFileSync(inputFile, 'utf8').split('\n');
+
+
+/* PART 1 */
+function part1() {
+
+   
+
+}
+
+
+/* PART 2 */
+function part2() {
+
+}
+
+
+console.log("Part 1: " + part1());
+console.log("Part 2: " + part2());
