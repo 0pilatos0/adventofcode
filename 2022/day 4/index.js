f4bde12d3d87a00057ce039ce4995c3538f56482
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-12-04 11:15:33 +0100

    🎄finished day 4 2022

--- 2022/day 4/index.js
+++ 2022/day 4/index.js
@@ -7,15 +7,50 @@ const input = fs.readFileSync(inputFile, 'utf8').split('\n');
 
 /* PART 1 */
 function part1() {
+    let totalOverlaps = 0;
+    input.forEach((line, index) => {
+        let firstElf = line.trim().split(',')[0];
+        let secondElf = line.trim().split(',')[1];
 
-   
+        let firstElfStart = parseInt(firstElf.split('-')[0]);
+        let firstElfEnd = parseInt(firstElf.split('-')[1]);
 
+        let secondElfStart = parseInt(secondElf.split('-')[0]);
+        let secondElfEnd = parseInt(secondElf.split('-')[1]);
+
+        if (firstElfStart >= secondElfStart && firstElfEnd <= secondElfEnd) {
+            totalOverlaps++;
+        }
+        if (secondElfStart >= firstElfStart && secondElfEnd <= firstElfEnd) {
+            totalOverlaps++;
+        }
+    });
+
+    return totalOverlaps;
 }
+
 
 
 /* PART 2 */
 function part2() {
+    let totalOverlaps = 0;
+    input.forEach((line, index) => {
+        let firstElf = line.trim().split(',')[0];
+        let secondElf = line.trim().split(',')[1];
 
+        let firstElfStart = parseInt(firstElf.split('-')[0]);
+        let firstElfEnd = parseInt(firstElf.split('-')[1]);
+
+        let secondElfStart = parseInt(secondElf.split('-')[0]);
+        let secondElfEnd = parseInt(secondElf.split('-')[1]);
+
+        if (firstElfEnd >= secondElfStart && secondElfEnd >= firstElfStart) {
+            totalOverlaps++;
+        } else if (secondElfEnd >= firstElfStart && firstElfEnd >= secondElfStart) {
+            totalOverlaps++;
+        }
+    });
+    return totalOverlaps
 }
 
 
