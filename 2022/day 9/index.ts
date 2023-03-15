08c0871a71f0c79e8d8fbc5c6927baf518d942b3
0pilatos0 <Paulvanderlei@hotmail.nl>
2023-03-15 22:14:18 +0100

    prepared project for typescript

---
+++ 2022/day 9/index.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+const inputFile = "input.txt";
+// const inputFile = "testInput.txt";
+
+const input = fs.readFileSync(inputFile, "utf8").trim().split("\r\n");
+
+function part1() {
+  console.log("Part 1: ", 0);
+}
+
+function part2() {
+  console.log("Part 2: ", 0);
+}
+
+part1();
+part2();
