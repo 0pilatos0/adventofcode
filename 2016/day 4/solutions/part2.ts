7aae9286988a6348eb3e42482691bd03506e34cb
0pilatos0 <paulvanderlei@hotmail.nl>
2023-10-29 13:03:00 +0100

    resolved day 4

---
+++ 2016/day 4/solutions/part2.ts
@@ -0,0 +1,63 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function part2(input: string): string {
+  const lines = input.split("\n");
+
+  type Line = {
+    string: string;
+    sectorId: number;
+    checksum: string;
+  };
+
+  const parsedLines: Line[] = lines.map((line) => {
+    const [string, sectorId, checksum] = line
+      .match(/([a-z-]+)-(\d+)\[(\w+)\]/)!
+      .slice(1);
+    return {
+      string,
+      sectorId: parseInt(sectorId),
+      checksum,
+    };
+  });
+
+  const validLines = parsedLines.filter((line) => {
+    const letters = line.string.replace(/-/g, "").split("");
+    const letterCount: Record<string, number> = {};
+    letters.forEach((letter) => {
+      letterCount[letter] = (letterCount[letter] || 0) + 1;
+    });
+    const letterCounts = Object.entries(letterCount).sort((a, b) => {
+      if (a[1] === b[1]) {
+        return a[0].localeCompare(b[0]);
+      }
+      return b[1] - a[1];
+    });
+    const checksum = letterCounts
+      .slice(0, 5)
+      .map((entry) => entry[0])
+      .join("");
+    return checksum === line.checksum;
+  });
+
+  const sectorIds = validLines.map((line) => line.sectorId);
+  const realRooms = validLines.map((line) => {
+    const letters = line.string.split("");
+    const decryptedLetters = letters.map((letter) => {
+      if (letter === "-") {
+        return " ";
+      }
+      const charCode = letter.charCodeAt(0);
+      const aCharCode = "a".charCodeAt(0);
+      const zCharCode = "z".charCodeAt(0);
+      const newCharCode =
+        ((charCode - aCharCode + line.sectorId) % 26) + aCharCode;
+      return String.fromCharCode(newCharCode);
+    });
+    return decryptedLetters.join("");
+  });
+  const northPoleRoom = realRooms.find((room) => room.includes("northpole"));
+  if (!northPoleRoom) {
+    throw new Error("No room found");
+  }
+  const northPoleRoomIndex = realRooms.indexOf(northPoleRoom);
+  return sectorIds[northPoleRoomIndex].toString();
+}
