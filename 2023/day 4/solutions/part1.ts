cc28cc0b9e07f31928a985b3c5b5dc2ecc7219da
Paul van der lei <54599584+0pilatos0@users.noreply.github.com>
2023-12-04 18:14:21 +0000

    🎄🎄 finished day 4 2023

---
+++ 2023/day 4/solutions/part1.ts
@@ -0,0 +1,40 @@
+export function part1(input: string): string {
+  type Card = {
+    cardNumber: number;
+    winningNumbers: number[];
+    myNumbers: number[];
+    points: number;
+  }
+  
+  const lines = input.split("\n");
+
+  const cards: Card[] = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const cardNumber = parseInt(line.split(":")[0].split(" ")[1]);
+    const winningNumbers = line.split("|")[0].split(": ")[1].split( /(?=(?:...)*$)/ ).map((x) => parseInt(x));
+    const myNumbers = line.split("|")[1].split(/(?=(?:...)*$)/).map((x) => parseInt(x));
+    const card = {
+      cardNumber: cardNumber,
+      winningNumbers: winningNumbers,
+      myNumbers: myNumbers,
+      points: 0,
+    };
+    cards.push(card);
+  }
+
+  let totalPoints = 0;
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    for (let j = 0; j < card.myNumbers.length; j++) {
+      const myNumber = card.myNumbers[j];
+      if (card.winningNumbers.includes(myNumber)) {
+        if (card.points > 0) card.points = card.points * 2;
+        else card.points = 1;
+      }
+    }
+    totalPoints += card.points;
+  }
+
+  return totalPoints.toString();
+}
---
+++ 2023/day 4/solutions/part2.ts
@@ -0,0 +1,52 @@
+export function part2(input: string): string {
+  type Card = {
+    cardNumber: number;
+    winningNumbers: number[];
+    myNumbers: number[];
+    copies: number;
+  }
+  
+  const lines = input.split("\n");
+
+  const cards: Card[] = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const cardNumber = parseInt(line.split(":")[0].split(" ")[1]);
+    const winningNumbers = line.split("|")[0].split(": ")[1].split( /(?=(?:...)*$)/ ).map((x) => parseInt(x));
+    const myNumbers = line.split("|")[1].split(/(?=(?:...)*$)/).map((x) => parseInt(x));
+    const card = {
+      cardNumber: cardNumber,
+      winningNumbers: winningNumbers,
+      myNumbers: myNumbers,
+      copies: 1,
+    };
+    cards.push(card);
+  }
+
+
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    for (let copy = 0; copy < card.copies; copy++) {
+      let totalMatches = 0;
+      for (let j = 0; j < card.myNumbers.length; j++) {
+        const myNumber = card.myNumbers[j];
+        if (card.winningNumbers.includes(myNumber)) {
+          totalMatches++;
+        }
+      }
+
+      //adding new copies
+      for (let j=1; j< totalMatches +1; j++) {
+        cards[i+j].copies = cards[i+j].copies + 1; //werkt wel
+        cards[i+j].copies = cards[i+j].copies++; // werkt niet
+      }
+    }
+  }
+
+  let totalCards = 0;
+  for (let i = 0; i < cards.length; i++) {
+    totalCards += cards[i].copies;
+  }
+
+  return totalCards.toString();
+}
