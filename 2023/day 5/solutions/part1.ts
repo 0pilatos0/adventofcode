17b4a1d18d34beafece7ea6b6b705665e337c6d6
Paul van der Lei <Paul.vanderlei@wics.nl>
2023-12-05 12:15:59 +0100

    🎄🎄 Finished day 5 part 1 2023

---
+++ 2023/day 5/solutions/part1.ts
@@ -0,0 +1,83 @@
+export function part1(input: string): string {
+  let seeds = input.split("\n")[0].split(": ")[1].split(" ").map(Number);
+  type map = {
+    from: string;
+    to: string;
+    mapEntries: MapEntry[];
+  };
+
+  type MapEntry = {
+    destinationRangeStart: number;
+    sourceRangeStart: number;
+    rangeLength: number;
+  };
+
+  let maps: map[] = [];
+
+  let inputArray = input.split("\n\n").map((x) => x.split("\n"));
+  inputArray.shift();
+
+  inputArray.forEach((x) => {
+    let from = x[0].split("-to-")[0];
+    let to = x[0].split("-to-")[1].split(" ")[0];
+
+    let mapEntries: MapEntry[] = [];
+    x.shift();
+    x.forEach((y) => {
+      let destinationRangeStart = Number(y.split(" ")[0]);
+      let sourceRangeStart = Number(y.split(" ")[1]);
+      let rangeLength = Number(y.split(" ")[2]);
+
+      mapEntries.push({
+        destinationRangeStart,
+        sourceRangeStart,
+        rangeLength,
+      });
+    });
+
+    let map: map = {
+      from,
+      to,
+      mapEntries,
+    };
+
+    maps.push(map);
+  });
+
+  let lowestLocationNumber = 9999;
+  for (let i = 0; i < seeds.length; i++) {
+    let location = seeds[i];
+    console.log(`seed: ${location}`);
+
+    let map = maps.find((x) => x.from == "seed");
+    while (map) {
+      console.log(`mapping: ${map.from} -> ${map.to}`);
+
+      let mapEntry = map.mapEntries.find(
+        (x) =>
+          location >= x.sourceRangeStart &&
+          location <= x.sourceRangeStart + x.rangeLength
+      );
+
+      if (mapEntry) {
+        location =
+          mapEntry.destinationRangeStart +
+          (location - mapEntry.sourceRangeStart);
+        console.log(`new location: ${location}`);
+      }
+
+      if (map?.to == "location") {
+        if (location < lowestLocationNumber) {
+          lowestLocationNumber = location;
+        }
+        map = undefined;
+      }
+
+      map = maps.find((x) => x.from == map?.to);
+    }
+
+    console.log(`final location: ${location}`);
+  }
+
+  return lowestLocationNumber.toString();
+}
