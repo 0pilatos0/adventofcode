50b3e12b326a5b79ab83cce91e291dba56718848
Paul van der Lei <Paul.vanderlei@wics.nl>
2023-12-06 10:35:42 +0100

    🎄🎄 Finished day 6 part 2

--- 2023/day 6/solutions/part2.ts
+++ 2023/day 6/solutions/part2.ts
@@ -1,4 +1,44 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function part2(input: unknown): string {
-  return "not implemented yet";
+export function part2(input: string): string {
+  type Race = {
+    time: number;
+    distance: number;
+  };
+  const lines = input.split("\n").map((x) => x.split(/\s+/));
+
+  lines[0].shift();
+  lines[1].shift();
+
+  lines[0][0] = lines[0].join("");
+  lines[1][0] = lines[1].join("");
+
+  let race = {
+    time: Number(lines[0][0]),
+    distance: Number(lines[1][0]),
+  };
+
+  let races: Race[] = [];
+  races.push(race);
+
+  let possibleRaces = [];
+
+  for (let raceIndex = 0; raceIndex < races.length; raceIndex++) {
+    let race = races[raceIndex];
+    let winCount = 0;
+    for (
+      let holdingDownTime = 0;
+      holdingDownTime <= race.time;
+      holdingDownTime++
+    ) {
+      let timeLeft = race.time - holdingDownTime;
+      let distanceReached = holdingDownTime * timeLeft;
+
+      if (distanceReached > race.distance) {
+        winCount++;
+      }
+    }
+
+    possibleRaces.push(winCount);
+  }
+
+  return possibleRaces.reduce((a, b) => a * b).toString();
 }
