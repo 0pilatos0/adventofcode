5dff9ceb3c0d9a1edc4646f5dd4385936375b013
0pilatos0 <paulvanderlei@hotmail.nl>
2023-12-09 12:04:46 +0100

    🎄🎄 finished day 9

---
+++ 2023/day 9/solutions/part2.ts
@@ -0,0 +1,36 @@
+export function part2(input: string): string {
+  const lines = input
+    .split("\n")
+    .map((line) => line.split(" ").map((item) => parseInt(item)));
+
+  const result = lines
+    .map((arr) => extrapolate(arr.toReversed()))
+    .reduce((acc, x) => acc + x, 0);
+  return result.toString();
+}
+
+function extrapolate(listOfNumbers: number[]): number {
+  const table: number[][] = [listOfNumbers];
+
+  for (let i = 1; hasNonZeroValues(table[i - 1]); i++) {
+    table.push(Array(listOfNumbers.length));
+
+    for (let j = i; j < listOfNumbers.length; j++) {
+      table[i][j] = calculateDifference(table[i - 1][j], table[i - 1][j - 1]);
+    }
+  }
+
+  return calculateSumOfLastElements(table);
+}
+
+function hasNonZeroValues(numbers: number[]): boolean {
+  return numbers.some((value) => value !== 0);
+}
+
+function calculateDifference(a: number, b: number): number {
+  return a - b;
+}
+
+function calculateSumOfLastElements(table: number[][]): number {
+  return table.reduce((acc, row) => acc + row.at(-1)!, 0);
+}
