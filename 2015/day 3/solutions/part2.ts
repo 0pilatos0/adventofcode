c8e6dbf8022eaafef8609d3610e693092a1f7318
0pilatos0 <paulvanderlei@hotmail.nl>
2024-08-27 23:13:14 +0200

    improved performance

--- 2015/day 3/solutions/part2.ts
+++ 2015/day 3/solutions/part2.ts
@@ -1,51 +1,33 @@
 export function part2(input: string): string {
   const visitedHouses = new Set<string>();
-  let santaX = 0;
-  let santaY = 0;
-  let robotSantaX = 0;
-  let robotSantaY = 0;
+  const players = [
+    [0, 0], //santa
+    [0, 0], //robot-santa
+  ];
 
-  visitedHouses.add(`${santaX},${santaY}`);
+  visitedHouses.add(`${players[0][0]},${players[0][1]}`);
 
   const directions = input.split("");
+  let currentPlayer = 0;
 
-  let even = true;
   for (const direction of directions) {
-    if (even) {
-      switch (direction) {
-        case "^":
-          santaY++;
-          break;
-        case "v":
-          santaY--;
-          break;
-        case ">":
-          santaX++;
-          break;
-        case "<":
-          santaX--;
-          break;
-      }
-      visitedHouses.add(`${santaX},${santaY}`);
-    } else {
-      switch (direction) {
-        case "^":
-          robotSantaY++;
-          break;
-        case "v":
-          robotSantaY--;
-          break;
-        case ">":
-          robotSantaX++;
-          break;
-        case "<":
-          robotSantaX--;
-          break;
-      }
-      visitedHouses.add(`${robotSantaX},${robotSantaY}`);
+    switch (direction) {
+      case "^":
+        players[currentPlayer][1]++;
+        break;
+      case "v":
+        players[currentPlayer][1]--;
+        break;
+      case ">":
+        players[currentPlayer][0]++;
+        break;
+      case "<":
+        players[currentPlayer][0]--;
+        break;
     }
 
-    even = !even;
+    visitedHouses.add(`${players[currentPlayer][0]},${players[currentPlayer][1]}`);
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
   }
 
   return visitedHouses.size.toString();
