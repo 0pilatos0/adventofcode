5573b4fef68cfc4bc60e13616d193d2a8d325a65
Paul van der Lei <paul.vanderlei@wics.nl>
2024-12-02 08:54:50 +0100

    🎄finished 2024 / day 2

--- 2024/day 2/solutions/part1.ts
+++ 2024/day 2/solutions/part1.ts
@@ -2,21 +2,29 @@ export function part1(input: string): string {
   const lines = input.split("\n").map((line) => line.split(" ").map((x) => parseInt(x, 10)));
   let safeReports = 0;
 
+  const validDiff = (diff: number): boolean => diff >= 1 && diff <= 3;
+  const validAccending = (valueA: number, valueB: number): boolean => valueA < valueB;
+  const validDescending = (valueA: number, valueB: number): boolean => valueA > valueB;
+
+  function compare(valueA: number, valueB: number, direction: "Ascending" | "Descending"): boolean {
+    const diff = Math.abs(valueA - valueB);
+    if (!validDiff(diff)) {
+      return false;
+    }
+
+    if ((direction === "Ascending" && validAccending(valueA, valueB)) || (direction === "Descending" && validDescending(valueA, valueB))) {
+      return false;
+    }
+
+    return true;
+  }
+
   for (const line of lines) {
     const direction: "Ascending" | "Descending" = line[1] > line[0] ? "Ascending" : "Descending";
     let valid = true;
-    for (let i = 1; i < line.length; i++) {
-      const diff = Math.abs(line[i] - line[i - 1]);
-      if (diff < 1 || diff > 3) {
-        valid = false;
-        break;
-      }
 
-      //heck if the direction is still correct
-      if (direction === "Ascending" && line[i] < line[i - 1]) {
-        valid = false;
-        break;
-      } else if (direction === "Descending" && line[i] > line[i - 1]) {
+    for (let i = 1; i < line.length; i++) {
+      if (!compare(line[i], line[i - 1], direction)) {
         valid = false;
         break;
       }
--- 2024/day 2/solutions/part2.ts
+++ 2024/day 2/solutions/part2.ts
@@ -2,37 +2,38 @@ export function part2(input: string): string {
   const lines = input.split("\n").map((line) => line.split(" ").map((x) => parseInt(x, 10)));
   let safeReports = 0;
 
+  const validDiff = (diff: number): boolean => diff >= 1 && diff <= 3;
+  const validAccending = (valueA: number, valueB: number): boolean => valueA < valueB;
+  const validDescending = (valueA: number, valueB: number): boolean => valueA > valueB;
+
+  function compare(valueA: number, valueB: number, direction: "Ascending" | "Descending"): boolean {
+    const diff = Math.abs(valueA - valueB);
+    if (!validDiff(diff)) {
+      return false;
+    }
+
+    if ((direction === "Ascending" && validAccending(valueA, valueB)) || (direction === "Descending" && validDescending(valueA, valueB))) {
+      return false;
+    }
+
+    return true;
+  }
+
   for (const line of lines) {
     const direction: "Ascending" | "Descending" = line[1] > line[0] ? "Ascending" : "Descending";
     let valid = true;
     let usedSkip = false;
+
     for (let i = 1; i < line.length; i++) {
-      const diff = Math.abs(line[i] - line[i - 1]);
-      if (diff < 1 || diff > 3) {
-        const diffWhileSkipping = Math.abs(line[i + 1] - line[i - 1]);
-
-        if (diffWhileSkipping < 1 || diffWhileSkipping > 3 || usedSkip) {
-          valid = false;
-          break;
+      if (!compare(line[i], line[i - 1], direction)) {
+        //check if when we skip the current value, the next value is valid with the previous value
+        if (!usedSkip && compare(line[i], line[i - 2], direction)) {
+          usedSkip = true;
+          continue;
         }
 
-        usedSkip = true;
-      }
-
-      if (direction === "Ascending" && line[i] < line[i - 1]) {
-        if (line[i + 1] < line[i - 1] || usedSkip) {
-          valid = false;
-          break;
-        }
-
-        usedSkip = true;
-      } else if (direction === "Descending" && line[i] > line[i - 1]) {
-        if (line[i + 1] > line[i - 1] || usedSkip) {
-          valid = false;
-          break;
-        }
-
-        usedSkip = true;
+        valid = false;
+        break;
       }
     }
 
