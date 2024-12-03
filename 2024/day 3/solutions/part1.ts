ff8278d8d3904c4cfee70b86080dfb5d2fcb236c
0pilatos0 <paulvanderlei@hotmail.nl>
2024-12-03 09:30:03 +0100

    🎄finished 2024 / day 3 (cleanup)

--- 2024/day 3/solutions/part1.ts
+++ 2024/day 3/solutions/part1.ts
@@ -1,17 +1,12 @@
 export function part1(input: string): string {
   const regex = /mul\([0-9]{1,3},[0-9]{1,3}\)/gm;
-  const matches = input.match(regex);
+  const instructions = input.match(regex);
   let count = 0;
 
-  for (const match of matches!) {
-    const [left, right] = match
-      .slice(4, -1)
-      .split(",")
-      .map((n) => parseInt(n, 10));
-
-    console.log(left, right, match);
+  instructions?.forEach((instruction) => {
+    const [left, right] = instruction.slice(4, -1).split(",").map(Number);
     count += left * right;
-  }
+  });
 
   return count.toString();
 }
--- 2024/day 3/solutions/part2.ts
+++ 2024/day 3/solutions/part2.ts
@@ -1,30 +1,19 @@
 export function part2(input: string): string {
   const regex = /mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)/gm;
-  const matches = input.match(regex);
+  const instructions = input.match(regex);
   let count = 0;
   let enabled = true;
 
-  for (const match of matches!) {
-    switch (match) {
-      case "don't()":
-        enabled = false;
-        continue;
-      case "do()":
-        enabled = true;
-        continue;
+  instructions?.forEach((instruction) => {
+    if (instruction === "don't()") {
+      enabled = false;
+    } else if (instruction === "do()") {
+      enabled = true;
+    } else if (enabled) {
+      const [left, right] = instruction.slice(4, -1).split(",").map(Number);
+      count += left * right;
     }
-
-    if (!enabled) {
-      continue;
-    }
-
-    const [left, right] = match
-      .slice(4, -1)
-      .split(",")
-      .map((n) => parseInt(n, 10));
-
-    count += left * right;
-  }
+  });
 
   return count.toString();
 }
