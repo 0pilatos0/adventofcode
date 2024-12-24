9ba6892fe9e2b9f3555e4fe2e0d64b00b6caeae3
0pilatos0 <paulvanderlei@hotmail.nl>
2024-12-24 22:38:15 +0100

    🎄finished 2024 / day 19

---
+++ 2024/day 19/solutions/part1.ts
@@ -0,0 +1,24 @@
+export function part1(input: string): string {
+  const data: string[] = input.split("\r\n\r\n");
+
+  const towels: string[] = data[0].split(", ");
+  const patterns: string[] = data[1].split("\r\n");
+  let answerp1: number = 0;
+
+  function memoize(partialPattern: string, towels: string[], memo: Map<string, number>): number {
+    if (memo.has(partialPattern)) return memo.get(partialPattern) as number;
+    if (!partialPattern.length) return 1;
+    const count: number = towels
+      .filter((towel: string) => partialPattern.startsWith(towel))
+      .reduce((sum: number, towel: string) => sum + memoize(partialPattern.slice(towel.length), towels, memo), 0);
+    memo.set(partialPattern, count);
+    return count;
+  }
+
+  patterns.forEach((pattern: string) => {
+    const totalCount: number = memoize(pattern, towels, new Map<string, number>());
+    if (totalCount) answerp1++;
+  });
+
+  return answerp1.toString();
+}
